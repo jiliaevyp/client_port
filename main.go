7febd952f4dc5f4aa7982aab07fcda1b16451c62
jiliaevyp <jiliaevyp@gmail.com>
2021-01-29 18:44:44 +0500

    client вводит тип сети IP сервера порт есть адреса по умолчанию 2 режима работы ручной-автоматический делает запрос получает ответ от сервера нужен выход по таймеру

--- main.go
+++ main.go
@@ -20,12 +20,129 @@ var (
 )
 
 const (
-	default_net  = "tcp"
-	default_IP   = "192.168.0.101"
-	default_port = "8181"
+	defaultNet  = "tcp"
+	defaultIp   = "192.168.1.101"
+	defaultPort = "8181"
 )
 
-// запрос
+// заголовок
+func _beg() {
+	fmt.Println("-------------------------------------")
+	fmt.Println("|        'Go' client-сервер         |")
+	fmt.Println("| Запрашиваем серверы до посинения! |")
+	fmt.Println("|                                   |")
+	fmt.Println("|   (c) jiliaevyp@gmail.com         |")
+	fmt.Println("-------------------------------------")
+}
+
+// установка конфигурации сервера
+func _config() (string, string) {
+	var err int
+	var ip, port, net string
+	err = 1
+	for err == 1 {
+		net, err = inpNetwork() // ввод сети сервера
+		fmt.Println("Тип сети:   ", net, "\n")
+	}
+	err = 1
+	for err == 1 {
+		ip, err = inpIP() // ввод IP сервера
+		fmt.Println("IP адрес сервера:   ", ip, "\n")
+	}
+	err = 1
+	for err == 1 {
+		port, err = inpPort() // ввод порта сервера
+		fmt.Println("Порт сервера:   ", port, "\n")
+	}
+	return net, ip + port
+}
+
+// ввод типа протокола сети
+func inpNetwork() (string, int) {
+	var typNet string
+	var err int
+	len := 256
+	err = 1
+	for err == 1 {
+		fmt.Print("Тип сети по умолчанию:	", defaultNet, "\n", " Нажмите (Y) для изменения ")
+		yes := yesNo()
+		if yes == 0 {
+			typNet = defaultNet
+			return typNet, 0
+		} else {
+			fmt.Print("Введите тип сети:	")
+			data := make([]byte, len)
+			n, err := os.Stdin.Read(data)
+			typNet = string(data[0 : n-1])
+			if err != nil || typNet != "tcp" {
+				fmt.Println(ErrInvalidTypeNetwork)
+				return typNet, 1
+			}
+			return typNet, 0
+		}
+	}
+	return typNet, 0
+}
+
+// ввод ip адреса сервера
+func inpIP() (string, int) {
+	data := ""
+	err := 1
+	for err == 1 {
+		fmt.Print("IP адрес сервера по умолчанию:	", defaultIp, "\n", "Для изменения нажмите 'Y' ")
+		yes := yesNo()
+		if yes != 1 {
+			data = defaultIp
+			err = 0
+		} else {
+			fmt.Print("Введите IP адрес сервера:	")
+			fmt.Scanf(
+				"%s\n",
+				&data,
+			)
+			iperr := net.ParseIP(data)
+			if iperr == nil {
+				fmt.Println(ErrInvalidIPaddress)
+				return data, 1
+			} else {
+				err = 0
+			}
+		}
+	}
+	return data, err
+}
+
+//ввод номера порта
+func inpPort() (string, int) {
+	var (
+		webPort string
+	)
+	err := 1
+	for err == 1 {
+		fmt.Print("Порт по умолчанию:	", defaultPort, "\n", "Для изменения нажмите 'Y' ")
+		yes := yesNo()
+		if yes != 1 {
+			webPort = defaultPort
+			err = 0
+		} else {
+			fmt.Print("Введите порт:	")
+			fmt.Scanf(
+				"%s\n",
+				&webPort,
+			)
+			res, err1 := strconv.ParseFloat(webPort, 16)
+			res = res + 1
+			err = 0
+			if err1 != nil {
+				fmt.Println(ErrInvalidPort)
+				return ":" + webPort, 1
+			}
+		}
+	}
+	return ":" + webPort, 0
+}
+
+// текст запроса
 func _textRequest() string {
 	var text string
 	len := 256
@@ -40,7 +157,7 @@ func _textRequest() string {
 	}
 }
 
-// проверка на окончание запросов Yes = 1
+// проверка на ввод  'Y = 1
 func yesNo() int {
 	var yesNo string
 	len := 4
@@ -54,86 +171,7 @@ func yesNo() int {
 	}
 }
 
-// ввод протокола сети
-func inpNetwork() (string, int) {
-	var typNet string
-	var err int
-	len := 256
-	err = 1
-	for err == 1 {
-		fmt.Print("Тип сети:	", default_net, "\n", " Нажмите (Y) для изменения ")
-		yes := yesNo()
-		if yes == 0 {
-			typNet = default_net
-			return typNet, 0
-		} else {
-			data := make([]byte, len)
-			n, err := os.Stdin.Read(data)
-			typNet = string(data[0 : n-1])
-			if err != nil || typNet != "tcp" {
-				fmt.Println(ErrInvalidTypeNetwork)
-				return typNet, 1
-			}
-			return typNet, 0
-		}
-	}
-	return typNet, 0
-}
-
-// ввод ip сервера
-func inpIP() (string, int) {
-	data := ""
-	err := 1
-	for err == 1 {
-		fmt.Print("Введите IP сервера:	", default_IP, "\n", " Нажмите (Y) для изменения ")
-		yes := yesNo()
-		if yes != 1 {
-			data = default_IP
-			err = 0
-		} else {
-			fmt.Scanf(
-				"%s\n",
-				&data,
-			)
-			iperr := net.ParseIP(data)
-			if iperr == nil {
-				fmt.Println(ErrInvalidIPaddress)
-				return data, 1
-			}
-		}
-	}
-	return data, err
-}
-
-//ввод номер порта
-func inpPort() (string, int) {
-	var (
-		webPort string
-	)
-	err := 1
-	for err == 1 {
-		fmt.Print("Введите порт:	", default_port, "\n", " Нажмите (Y) для изменения ")
-		yes := yesNo()
-		if yes != 1 {
-			webPort = default_port
-			err = 0
-		} else {
-			fmt.Scanf(
-				"%s\n",
-				&webPort,
-			)
-			res, err1 := strconv.ParseFloat(webPort, 16)
-			res = res + 1
-			err = 0
-			if err1 != nil || len(webPort) != 4 {
-				fmt.Println(ErrInvalidPort)
-				return ":" + webPort, 1
-			}
-		}
-	}
-	return ":" + webPort, 0
-}
-
+// ввод числа запросов в автоматическом режиме
 func zikly() int {
 	var s string
 	var n int
@@ -150,6 +188,7 @@ func zikly() int {
 		} else {
 			err1 = 0
 			n = res
+			return n
 		}
 	}
 	return n
@@ -157,24 +196,22 @@ func zikly() int {
 
 // client server
 // сервер запущен на локальном компьтере на порте 4545
-// клиент подлючается к этому адресу: net.Dial("tcp", ":4545")
-// серверу будет отправляться запрос conn.Write([]byte(source))
+// клиент подлючается к этому адресу: net.Dial(_network, "_ip:_port")
+// серверу будет отправляться запрос '_text' через conn.Write([]byte(source))
 // с помощью вызова conn.Read выводим полученный ответ на консоль.
 
-func _client(_text string) (string, int) {
+func _client(requestMessage string) (string, int) {
 	answer := ""
 	conn, err := net.Dial(_network, _port)
 	if err != nil {
 		fmt.Println(err)
 		return "", 1
 	}
-	defer conn.Close()
-	requestMessage = _text
+	defer conn.Close() // закрыть соединение в конце
 	if n, err := conn.Write([]byte(requestMessage)); n == 0 || err != nil {
 		fmt.Println(ErrInvalidSendToServer)
 		return "", 1
 	}
-	fmt.Println("Ответ сервера: ")
 	buff := make([]byte, 1024)
 	n, err := conn.Read(buff)
 	if err != nil {
@@ -185,46 +222,22 @@ func _client(_text string) (string, int) {
 	return answer, 0
 }
 
-func _beg() {
-	fmt.Println("------------------------------------")
-	fmt.Println("|          Запуск Go client        |")
-	fmt.Println("|     Запрашиваем до посинения!    |")
-	fmt.Println("|                                  |")
-	fmt.Println("|   (c) jiliaevyp@gmail.com        |")
-	fmt.Println("------------------------------------")
-}
-
 func main() {
-	var _answer string
+	var _answer, is string
 	var err int
 
-	_beg() // заголовок
-	err = 1
-	for err == 1 {
-		_network, err = inpNetwork() // ввод сети сервера
-		fmt.Println(_network)
-	}
-	err = 1
-	for err == 1 {
-		_ip, err = inpIP() // ввод IP сервера
-		fmt.Println(_ip)
-	}
-	err = 1
-	for err == 1 {
-		_port, err = inpPort() // ввод порта сервера
-		fmt.Println(_port)
-	}
-	_port = _ip + _port
+	_beg()                      // заголовок
+	_network, _port = _config() // конфтигурация сервера
 	zikl := 1
 	for zikl == 1 {
 		fmt.Println("\n")
-		fmt.Println("Сервер:   ", _network, _port)
-		fmt.Println("Выберите режим запросов: ручной/автоматический")
-		fmt.Println("Нажмите 'Y' для ручного запроса")
+		fmt.Println("Конфигурация сервера:   ", _network, _port, "\n")
+		fmt.Println("Выберите режим запросов: одиночный/автоматический", "\n")
+		fmt.Println("Нажмите 'Y' для формирования одного запроса  (для перехода в автоматический режим - любая клавиша)", "\n")
 		yes := yesNo()
 		if yes == 1 {
 			request := 1 // ручные запросы
-			fmt.Println("Ручной режим")
+			fmt.Println("Режим одиночных запросов")
 			for request == 1 {
 				if request == 1 {
 					text := _textRequest()
@@ -237,27 +250,31 @@ func main() {
 				} else {
 					fmt.Println("Выход из режима одиночных запросов")
 				}
-				fmt.Println("Ручной режим, нажмите 'Y' для нового запроса")
+				fmt.Println("Режим одиночных запросов ", "\n", "нажмите 'Y' для продолжения (выход из режима - любая клавиша)")
 				request = yesNo()
 			}
 			yes = 0
 		} else { // автоматические запросы
 			fmt.Println("Режим автоматических запросов")
 			n := zikly()
-			_text := _textRequest()
+			text := _textRequest()
 			for i := 1; i < n; i++ {
-				fmt.Println("Запрос ", i)
+				is = strconv.Itoa(i)
+				//fmt.Println(i, is)
+				_text := is + " " + text
+				fmt.Println(is, "запрос: ", _text)
 				_answer, err = _client(_text)
 				if err != 0 {
 					fmt.Println(ErrInvalidServerRead)
 					break
 				}
+				fmt.Println("Ответ сервера:")
 				fmt.Println(_answer)
 			}
-			fmt.Println("Конец опросов")
+			fmt.Println("Конец запросов")
 			fmt.Println("Выход из режима автоматических запросов")
 		}
-		fmt.Println("Нажмите 'Y' для продолжения работы")
+		fmt.Println("Нажмите 'Y' для продолжения работы  (выход - любая клавиша)")
 		zikl = yesNo()
 	}
 	fmt.Println("Был счастлив на Вас поработать!")
